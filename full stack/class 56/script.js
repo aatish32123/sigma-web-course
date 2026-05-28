d383483fa73aa2e79a84829b60c8b6b8184017ce
aamir khan <amirkhan4532123@gmail.com>
2026-05-28 23:48:17 +0500

    key up

---
+++ full stack/class 56/script.js
@@ -0,0 +1,40 @@
+console.log ( "Hello i conditional tutorial" ) ;
+let age = 1 ;
+let grace = 2 ;
+
+// console.log ( age + grace ) ;
+// console.log ( age - grace ) ;
+// console.log ( age * grace ) ;
+// console.log ( age / grace ) ;
+// console.log ( age ** grace ) ; 
+// console.log ( age % grace ) ;
+
+
+
+
+
+
+if ( age == 18 ) {
+    console.log ( "You can drive" ) ;
+}
+else if ( age == 0 ) {
+    console.log ( "are you kidding me" ) ;
+}
+else if ( age == 1 ) {
+    console.log ( "Are you Again kidding me" ) ;
+}
+else {
+    console.log ( "You cannot drive" ) ;
+}
+
+a = 10 ;
+b = 20 ;
+let c = a > b ? (a -b) : (b-a) ;
+console.log ( c ) ;
+
+
+
+
+
+Translate
+
---
+++ full stack/class 57/index.js
@@ -0,0 +1,22 @@
+console .log ( "I am tutorial in loops" ) ;
+let a = 1;
+// console .log ( a + 1 ) ;
+
+for ( let i = 0 ; i < 100 ; i++ ) {
+    console .log ( a + i ) ;
+}
+
+
+let object = {
+    name : "AAtish" ,
+    age : 24 ,
+    city : "isb"
+} ;
+
+for ( const key in object ) {
+    console .log ( key ) ;
+    // console .log (object [ key ] ) ;
+}
+for ( const c of "Atish" ) {
+    console .log ( c ) ;
+}
